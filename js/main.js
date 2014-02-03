62041bd369e7ad8ce0707ab43d8f88f1f5d596db
mdrobny <michdrob@gmail.com>
2014-02-03 19:32:52 +0100

    Liczba mnoga w urlach

--- js/main.js
+++ js/main.js
@@ -3,100 +3,147 @@ $( document ).ready(function() {
     var apiUrl = "http://localhost/projekt-db/api/public/";
     var content = $('#content'),
     pageHeader = $('#header h1');
- 
-    var renderMieszkanie = function() {
-        $.ajax({
-            type: "GET",
-            url: apiUrl +"mieszkanie",
-            success: function( data ) {
-                $.get('mieszkanie.html', function (htmlTemplate) {
-                    content.html(_.template(htmlTemplate, {
-                        list: data.mieszkania
-                    }));
-                });
-            }
-        });
-    };
-    $('#nav-mieszkanie').click(function () {
-        pageHeader.text('Mieszkania');
-        renderMieszkanie();
-    });
+    
+    
+    
     /**
-     * Add record
+     * Mieszkanie
      */
+    var mieszkania = {
+        headerText: 'Mieszkania',
+        
+        render: function() {
+            pageHeader.text(this.headerText);
+            $.ajax({
+                type: "GET",
+                url: apiUrl +"mieszkania",
+                success: function( data ) {
+                    $.get('mieszkania.html', function (htmlTemplate) {
+                        content.html(_.template(htmlTemplate, {
+                            list: data.mieszkania
+                        }));
+                    });
+                }
+            });
+        }
+    };
+ 
+    $('#nav-mieszkania').click(function () {
+        mieszkania.render();
+    });
+    /** Add record **/
     content.on('submit', '#mieszkanie-form', function (event) {
         event.preventDefault();
         var data = $(this).serializeObject();
-        console.log('submit')
         $.ajax({
             type: "POST",
-            url: apiUrl +"mieszkanie",
+            url: apiUrl +"mieszkania",
             data: data,
-            success: function(data) {
-                renderMieszkanie();
+            success: function() {
+                mieszkania.render();
             }
         })
     });
+    /** Delete record **/
     content.on('click', '.mieszkanie-delete', function (event) {
         event.preventDefault();
         var id = $(event.target).data('id');
         $.ajax({
             type: "DELETE",
-            url: apiUrl +"mieszkanie/"+ id,
-            success: function(data) {
-                renderMieszkanie();
+            url: apiUrl +"mieszkania/"+ id,
+            success: function() {
+                mieszkania.render();
             }
         });
     });
    
- /*
+ /****************************************************************************
   * Osoby
   */
-  $('#nav-osoby').click(function () {
-        pageHeader.text('Osoby');
+    var osoby = {
+        headerText: 'Osoby',
+        
+        render: function() {
+            pageHeader.text(this.headerText);
+            $.ajax({
+                type: "GET",
+                url: apiUrl +"osoby",
+                success: function( data ) {
+                    $.get('osoby.html', function (htmlTemplate) {
+                        content.html(_.template(htmlTemplate, {
+                            list: data.osoby
+                        }));
+                    });
+                }
+            });
+        }
+    };
+     $('#nav-osoby').click(function () {
+        osoby.render();
+    });
+    /** Add record **/
+    content.on('submit', '#osoba-form', function (event) {
+        event.preventDefault();
+        var data = $(this).serializeObject();
+        console.log('submit')
+        console.log(data)
         $.ajax({
-            type: "GET",
+            type: "POST",
             url: apiUrl +"osoby",
-            success: function( data ) {
-                $.get('osoby.html', function (htmlTemplate) {
-                    content.html(_.template(htmlTemplate, {
-                        list: data.osoby
-                    }));
-                });
+            data: data,
+            success: function() {
+                osoby.render();
+            }
+        })
+    });
+    /** Delete record **/
+    content.on('click', '.osoba-delete', function (event) {
+        event.preventDefault();
+        var id = $(event.target).data('id');
+        $.ajax({
+            type: "DELETE",
+            url: apiUrl +"osoby/"+ id,
+            success: function() {
+                osoby.render();
             }
         });
     });
-    $('#osoby-form').delegate('submit', function (event) {
-        console.log('submit')
-        event.preventDefault();
-    });
- 
 
 
-/*
+/***************************************************************************
   * Ksieza
   */
  
-  $('#nav-ksieza').click(function () {
-        pageHeader.text('Księża');
-        $.ajax({
-            type: "GET",
-            url: apiUrl +"ksieza",
-            success: function( data ) {
-                $.get('ksieza.html', function (htmlTemplate) {
-                    content.html(_.template(htmlTemplate, {
-                       list: data.ksieza
-                    }));
-                });
-            }
-        });
-    });
-    $('#ksieza-form').delegate('submit', function (event) {
-        console.log('submit')
-        event.preventDefault();
-    });
+//  $('#nav-ksieza').click(function () {
+//        pageHeader.text('Księża');
+//        $.ajax({
+//            type: "GET",
+//            url: apiUrl +"ksieza",
+//            success: function( data ) {
+//                $.get('ksieza.html', function (htmlTemplate) {
+//                    content.html(_.template(htmlTemplate, {
+//                       list: data.ksieza
+//                    }));
+//                });
+//            }
+//        });
+//    });
+//    $('#ksieza-form').delegate('submit', function (event) {
+//        console.log('submit')
+//        event.preventDefault();
+//    });
  
 });
+
+
+
+
+
+
+
+
+
+
 /**
  * Don't move or copy
  * @returns {String|Array|$.fn.serializeObject@pro;value}
